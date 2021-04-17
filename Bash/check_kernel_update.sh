81aa270f6999212c2d7781e86ece921555f526ee
zhang.j.k <zhangjk67@gmail.com>
2021-04-18 00:04:00 +0800

    modify scripts to fit bash and KDE

--- Bash/check_kernel_update.sh
+++ Bash/check_kernel_update.sh
@@ -1,11 +1,17 @@
-# e.g. linux 4.18.5.arch1-1
-set installed (string match --regex '\d\d?.\d\d?.\d\d?' (pacman --query linux))
+# 灵感来自 https://www.reddit.com/r/archlinux/comments/d1lj0a/how_come_arch_linux_can_upgrade_the_kernel/ezp2gii/ 的fish脚本
+# 将脚本改为bash，并配置KDE的通知
+# 根据是否更新了内核提醒用户重启
 
-# e.g. 4.18.4-arch1-1-ARCH
-set running (string match --regex '\d\d?.\d\d?.\d\d?' (uname --kernel-release))
+set -e
+# e.g. linux-lts 5.10.31-1
+installed=$((pacman --query linux)|grep -oP '\d\d?.\d\d?.\d\d?' )
+
+# e.g. 5.10.31-1-lts
+running=$((uname --kernel-release)|grep -oP '\d\d?.\d\d?.\d\d?')
 
 if test ! $running = $installed
+then
 
-    printf "restart!"
-
-end
+    echo 'You may need to restart since your kernel has been updated. ' | wall
+    notify-send 'You may need to restart since your kernel has been updated. 由于您更新了内核，你可能需要重启'
+fi
